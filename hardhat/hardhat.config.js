c882725636c4eb6eb9f90df145cacb572815326f
Disha Sathavara <dishasathavara1998@gmail.com>
2023-07-07 11:13:32 +0530

    deployed in ioTEX network

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -4,6 +4,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config();
 // require("dotenv").config({ path: "./.env" });
 
+ 
 
 module.exports = {
   solidity: "0.8.16",
@@ -11,8 +12,12 @@ module.exports = {
     apothem: {
       url: process.env.NEXT_PUBLIC_APOTHEM_NETWORK_URL,
       accounts: [process.env.NEXT_PUBLIC_PRIVATE_KEY]
-    }
-  },
+    },
+    testnet: {
+      url: `https://babel-api.testnet.iotex.io`,
+      accounts: [`${process.env.NEXT_PUBLIC_IOTEX_PRIVATE_KEY}`],
+    },
+  }
 };
 
 
